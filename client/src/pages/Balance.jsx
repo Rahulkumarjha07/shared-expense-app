7382d9b2b8d46378879d065ba737c9aa8d0ca48e
rahulkumarjha <rahulkumarjha523166@gmail.com>
2026-06-14 23:32:03 +0530

    Enhance user interface and navigation

--- client/src/pages/Balance.jsx
+++ client/src/pages/Balance.jsx
@@ -1,5 +1,62 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+import Layout from "../components/Layout";
+
 function Balance() {
-  return <h1 style={{padding:"30px"}}>📊 Balance Page</h1>;
+
+  const [balances, setBalances] = useState([]);
+
+  const loadBalances = async () => {
+    try {
+      const res = await API.get("/balance/1");
+      setBalances(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    loadBalances();
+  }, []);
+
+  return (
+    <Layout>
+
+      <h2>Balance Summary</h2>
+
+      <table className="table table-bordered mt-3">
+
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Paid</th>
+            <th>Owes</th>
+            <th>Net Balance</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          {Array.isArray(balances) &&
+            balances.map((b, index) => (
+              <tr key={index}>
+                <td>{b.name}</td>
+                <td>₹{b.total_paid}</td>
+                <td>₹{b.total_share}</td>
+                <td>
+                  <strong>
+                    ₹{Number(b.total_paid) - Number(b.total_share)}
+                  </strong>
+                </td>
+              </tr>
+            ))}
+
+        </tbody>
+
+      </table>
+
+    </Layout>
+  );
 }
 
 export default Balance;
