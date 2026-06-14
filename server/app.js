00bf1fe5894a69fe6615a7cadcd12f55076515e8
rahulkumarjha <rahulkumarjha523166@gmail.com>
2026-06-14 11:10:53 +0530

    Implement user registration API

--- server/app.js
+++ server/app.js
@@ -1,19 +1,25 @@
-    require("dotenv").config();
-    require("./config/db");
-    const express = require("express");
-    const cors = require("cors");
+require("dotenv").config();
+require("./config/db");
 
-    const app = express();
+const express = require("express");
+const cors = require("cors");
 
-    app.use(cors());
-    app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
 
-    app.get("/", (req, res) => {
-        res.send("Shared Expense API Running...");
-    });
+const app = express();
 
-    const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+// Register auth routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Shared Expense API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
